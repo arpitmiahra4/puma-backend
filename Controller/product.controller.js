b6277e6603694611530f74ecb30227e390e948cb
spsanchore13 <shantilalpatliya18@gmail.com>
2022-12-23 20:03:34 +0530

    page fix

--- Controller/product.controller.js
+++ Controller/product.controller.js
@@ -9,8 +9,8 @@ const getTrending = async (req, res) => {
 const getProduct = async (req, res) => {
     // console.log(req.query)
     const { category } = req.query
-    const page = parseInt(req.query.page) || 1
-    const limit = parseInt(req.query.limit) || 50
+    const page = parseInt(req.query.page) || 0
+    const limit = parseInt(req.query.limit) || 100
     const skip = limit * page;
     const product = await ProductModel.find({ category: category }).skip(skip).limit(limit)
     // const totalPage = Math.ceil(product.length / limit);
